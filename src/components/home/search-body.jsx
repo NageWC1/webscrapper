5f91480a57a139d782193b2858ab01cc7f853b75
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-09-29 19:00:09 +0530

    designed the search section, working on further designs

--- src/components/home/search-body.jsx
+++ src/components/home/search-body.jsx
@@ -1,11 +1,16 @@
-function SearchBody(){
-    return(
+import "../../styles/side-bar.css";
+function SearchBody() {
+  return (
     <>
-    <div className="search-body-wrapper">
-        <input placeholder="Enter the web link 🌐"/>
-    </div>
+      <div className="search-body-wrapper">
+          <input
+            placeholder="Enter the web link.... https://www/example.com"
+            className="search-element"
+          />
+          <button className="search-btn">Get</button>
+        </div>
     </>
-    )
+  );
 }
 
 export default SearchBody;
