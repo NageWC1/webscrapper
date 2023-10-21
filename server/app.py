554ac1b4d43fe12b746d34ee837415a0c51195b7
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-10-21 15:13:38 +0530

    able to ge the table from the site and everything working peperfectly

--- server/app.py
+++ server/app.py
@@ -1,16 +1,46 @@
 from flask import Flask, request , render_template
+import requests
 import pandas as pd
+from bs4 import BeautifulSoup
+
 import json
 app = Flask('__name__')
 
 @app.route('/', methods=['POST', 'GET'])
 def api():
-    link = request.form['link']
-    d = {'col1': [1, 2], 'col2': [3, 4]}
-    df = pd.DataFrame(data=d)
-    print(link)
-    table = df.to_html(classes="table")
-    return link
+    url = request.form['link']
+    # check whether website givig permision to scrape their data
+    per = check_permission(url)
+
+    if per == "ok":
+        table = scrap_table(url)
+    else:
+        table = "nothing"
+        
+    return table
+
+def scrap_table(link):
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, "lxml")
+    table = soup.find("table")
+    headers = table.find_all("th")
+    titles = []
+    for i in headers:
+        text = i.text
+        titles.append(text)
+    df = pd.DataFrame(columns=titles)
+    # table = df.to_html(classes="table")
+    return df.to_html()
+
+def check_permission(link):
+    permision = requests.get(link)
+
+    
+    if permision.status_code == 200:
+        per = "ok"
+    else:
+        per = "nope"
+    return per
 
 if __name__ == "__main__":
     app.run()
