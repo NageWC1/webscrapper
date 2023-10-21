c50b5cb5322ef2a9cf52261775119d6ebcca42d2
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-10-21 13:01:05 +0530

    find the way to send the html table to front end

--- server/app.py
+++ server/app.py
@@ -1,8 +1,14 @@
 from flask import Flask, request , render_template
-
+import pandas as pd
+import json
 app = Flask('__name__')
 
 @app.route('/', methods=['POST', 'GET'])
 def api():
-    data = request.form['link']
-    return data
+    d = {'col1': [1, 2], 'col2': [3, 4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    return df.to_html(classes="table")
+
+if __name__ == "__main__":
+    app.run()
