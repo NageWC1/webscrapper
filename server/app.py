0bd0d10aeb9be1c09f35524c7ae8b19c7a5d5461
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-10-21 14:14:34 +0530

    added the beautiful soup

--- server/app.py
+++ server/app.py
@@ -5,10 +5,12 @@ app = Flask('__name__')
 
 @app.route('/', methods=['POST', 'GET'])
 def api():
+    link = request.form['link']
     d = {'col1': [1, 2], 'col2': [3, 4]}
     df = pd.DataFrame(data=d)
-    print(df)
-    return df.to_html(classes="table")
+    print(link)
+    table = df.to_html(classes="table")
+    return link
 
 if __name__ == "__main__":
     app.run()
